f383dee93f9336f4afa2b4a46abc407437c957e6
prakor <warunyoo084@gmail.com>
2024-01-26 16:24:06 +0700

    26/01/2024

--- controllers/company/company.controllers.js
+++ controllers/company/company.controllers.js
@@ -21,22 +21,164 @@ const {
 } = require("../../funtions/uploadfilecreate");
 
 exports.create = async (req, res) => {
-    try {
-      const company = await Company.findOne({
-        Branch_company_name: req.body.Branch_company_name,
+  try {
+    const company = await Company.findOne({
+      Branch_company_name: req.body.Branch_company_name,
+    });
+    if (company)
+      return res.status(409).send({
+        status: false,
+        message: "มีชื่อบริษัทนี้อยู่ในระบบเเล้ว",
       });
-      if (company)
-        return res.status(409).send({
-          status: false,
-          message: "มีชื่อบริษัทนี้อยู่ในระบบเเล้ว",
+    await new Company({
+      ...req.body,
+    }).save();
+    res.status(201).send({ message: "เพิ่มข้อมูลบริษัทสำเร็จ", status: true });
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ status: false, message: "มีบางอย่างผิดพลาด" });
+  }
+};
+exports.ImportBankCompany = async (req, res) => {
+  try {
+    let upload = multer({ storage: storage }).array("imgCollection", 20);
+    upload(req, res, async function (err) {
+      const reqFiles = [];
+      const result = [];
+      if (err) {
+        return res.status(500).send(err);
+      }
+      if (req.files) {
+        const url = req.protocol + "://" + req.get("host");
+        for (var i = 0; i < req.files.length; i++) {
+          const src = await uploadFileCreate(req.files, res, { i, reqFiles });
+          result.push(src);
+        }
+      }
+      const id = req.params.id;
+      if (id && !req.body.password) {
+        const company = await Company.findByIdAndUpdate(id, {
+          ...req.body,
+          "bank.name": req.body.name,
+          "bank.number": req.body.number,
+          "bank.image": reqFiles[0],
         });
-      await new Company({
-        ...req.body,
-      }).save();
-      res.status(201).send({ message: "เพิ่มข้อมูลบริษัทสำเร็จ", status: true });
-    } catch (err) {
+        if (company) {
+          return res.status(200).send({
+            message: "เพิ่มรูปภาพสำเร็จ",
+            status: true,
+          });
+        } else {
+          return res.status(500).send({
+            message: "ไม่สามารถเพิ่มรูปภาพได้",
+            status: false,
+          });
+        }
+      }
+    });
+  } catch (error) {
+    return res.status(500).send({ status: false, error: error.message });
+  }
+};
+exports.EditCompany = async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!req.body) {
       return res
-        .status(500)
-        .send({ status: false, message: "มีบางอย่างผิดพลาด" });
+        .status(400)
+        .send({ status: false, message: error.details[0].message });
     }
-  };
+    const new_supplier = await Company.findByIdAndUpdate(id, {
+      ...req.body,
+    });
+    if (new_supplier) {
+      return res.send({
+        status: true,
+        message: "เเก้ไขข้อมูลบริษัทสำเร็จ",
+      });
+    } else {
+      return res.status(400).send({
+        status: false,
+        message: "เเก้ไขข้อมูลบริษัทไม่สำเร็จ",
+      });
+    }
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ status: false, message: "มีบางอย่างผิดพลาด" });
+  }
+};
+exports.deleteCompany = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const company = await Company.findByIdAndDelete(id);
+    if (!company) {
+      return res
+        .status(404)
+        .send({ status: false, message: "ไม่พบบริษัทในระบบ" });
+    } else {
+      return res.status(200).send({ status: true, message: "ลบข้อมูลสำเร็จ" });
+    }
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ status: false, message: "มีบางอย่างผิดพลาด" });
+  }
+};
+exports.deleteAllCompany = async (req, res) => {
+  try {
+    const result = await Company.deleteMany({});
+
+    if (result.deletedCount > 0) {
+      return res
+        .status(200)
+        .send({ status: true, message: "ลบข้อมูลบริษัททั้งหมดสำเร็จ" });
+    } else {
+      return res
+        .status(404)
+        .send({ status: false, message: "ไม่พบข้อมุลสาขา" });
+    }
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ status: false, message: "มีบางอย่างผิดพลาด" });
+  }
+};
+exports.getCompannyAlls = async (req, res) => {
+  try {
+    const companny = await Company.find();
+    if (!companny) {
+      return res
+        .status(404)
+        .send({ status: false, message: "ไม่พบข้อมูลบริษัทในระบบ" });
+    } else {
+      return res
+        .status(200)
+        .send({ status: true, message: "ดึงข้อมูลสำเร็จ", data: companny });
+    }
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ status: false, message: "มีบางอย่างผิดพลาด" });
+  }
+};
+exports.getCompanyById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const companny = await Company.findById(id);
+    if (!companny) {
+      return res
+        .status(404)
+        .send({ status: false, message: "ไม่พบข้อมูลบริษัทในระบบ" });
+    } else {
+      return res
+        .status(200)
+        .send({ status: true, message: "ดึงข้อมูลสำเร็จ", data: companny });
+    }
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ status: false, message: "มีบางอย่างผิดพลาด" });
+  }
+};
--- router/Company/index.js
+++ router/Company/index.js
@@ -5,7 +5,11 @@ const authAdmin = require("../../lib/auth.admin")
 
 
 router.post("/createCompany",authAdmin, Company.create)
-
-
+router.put("/ImportBankCompany/:id",authAdmin, Company.ImportBankCompany)
+router.put("/EditCompany/:id",authAdmin, Company.EditCompany)
+router.delete("/deleteCompany/:id",authAdmin, Company.deleteCompany)
+router.delete("/deleteAllCompany",authAdmin, Company.deleteAllCompany)
+router.get("/getCompannyAll",authAdmin, Company.getCompannyAlls)
+router.get("/getCompanyBy/:id",authAdmin, Company.getCompanyById)
 
 module.exports = router;
