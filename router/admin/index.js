c60b7cc9d60b240e4af5187ce1d9cfc1e355c8cd
prakor <warunyoo084@gmail.com>
2024-01-27 15:50:05 +0700

    Update index.js

--- router/admin/index.js
+++ router/admin/index.js
@@ -4,7 +4,7 @@ const authAdmin = require("../../lib/auth.admin");
 
 router.post("/create", authAdmin, admin.create);
 router.put("/EditAdmin/:id", authAdmin, admin.EditAdmin);
-router.delete("/EditAdmin/:id", authAdmin, admin.deleteAdmins);
+router.delete("/deleteAdmins/:id", authAdmin, admin.deleteAdmins);
 router.get("/getAdminAll", authAdmin, admin.getAdminAll);
 router.get("/getAdminBy/:id", authAdmin, admin.getAdminsById);
 module.exports = router;
