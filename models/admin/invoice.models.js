536a217005fe12c7a5e9e4783545c2f7232737d4
prakor <warunyoo084@gmail.com>
2024-01-30 16:49:42 +0700

    30/01/2024

--- models/admin/invoice.models.js
+++ models/admin/invoice.models.js
@@ -24,6 +24,7 @@ const InvoiceSchema = new mongoose.Schema({
     customer_type: { type: String, required: false }, //ประเภทลุกค้า
     customer_email: { type: String, required: false }, //email ลูกค้า
     customer_contact: { type: String, required: false }, //ข้อมูลติดต่อลูกค้า
+    customer_contact_number: { type: String, required: false }, //เบอร์โทรติดต่อผู้ประสานงาน
   },
   product_detail: [
     {
